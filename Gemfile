2038fbbb2dc94726faae5823d066965e1887faca
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2023-04-23 18:31:53 -0300

    model_user: add validações e specs

--- Gemfile
+++ Gemfile
@@ -3,16 +3,12 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.1.0'
 
-# Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.4', '>= 7.0.4.2'
-
-# Use postgresql as the database for Active Record
 gem 'bootsnap', require: false
 gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
-
-# Auth
+gem 'enumerize'
 gem 'devise_token_auth'
 
 group :development, :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,23 @@
 # frozen_string_literal: true
 
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+  # Constants
+  KINDS = %i[associate coach financial].freeze
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  # Validations
+  validates :name, :kind, :email, :password, :password_confirmation,
+    presence: true
+
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+
+  # Enumerize
+  extend Enumerize
+  enumerize :kind, in: KINDS, predicates: true
 end
