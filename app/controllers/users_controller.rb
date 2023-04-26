97dbe9d791547039fb1b9d2f2fcc9b6a72e6faeb
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2023-04-25 21:33:42 -0300

    add controller user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  include Authenticable
+
+  # GET /users
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  # GET /users/1
+  def show
+    render json: @user
+  end
+end
